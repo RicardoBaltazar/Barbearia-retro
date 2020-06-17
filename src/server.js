476e3cd8c4ee85022faddb4b32cafcf908b9e392
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-17 17:24:40 -0300

    criando rotas

--- src/server.js
+++ src/server.js
@@ -2,25 +2,39 @@
 //npm install express -save --> o -save serve para trazer o express para dentro da aplicação e facilitar para quem baixar
 //iniciar o servidor com -> node server.js
 //npm install nodemon -d
+//npm install mysql --save
+//npm install body-parser --save
 //nodemon server.js
 const express = require('express')
 const server = express()
+let bodyParser = require('body-parser')
 
-server.use(express.static('public'));
+server.use(express.static('../public'));
+
+//server.use(bodyParser.urlencoded({extended: true}))
+
 
 //habilitar o uso do req.body na nossa aplicação
 server.use(express.urlencoded({extended: true}))
 
 
+
 server.get('/', function(req, res){
     //res.send("index.html")
-    return res.sendFile(__dirname+ "/src/views/index.html")
+    return res.sendFile(__dirname+ "/views/index.html")
 })
 
 server.get('/create', function(req, res){
     //res.send("index.html")
-    return res.sendFile(__dirname+ "/src/views/create.html")
+    console.log(req.query)
+    return res.sendFile(__dirname+ "/views/create.html")
 })
+
+server.post("/savepoint", function(req,res){
+    //req.body: o corpo do nosso formulário
+    console.log(req.body)
+})
+
 
 server.listen(3000, function(){
     console.log('Servidor rodando')
