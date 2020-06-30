50e0a5923f02c026849fb6731877d9ff498c1709
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-30 15:31:02 -0300

    pagina home e page 2

---
+++ scripts/services.js
@@ -0,0 +1,11 @@
+let divHair = document.querySelector('#div-hair')
+let divBeard = document.querySelector('#div-beard')
+
+let valueHair = 20
+let valueBeard = 15
+
+let totalValue = 0
+
+divHair.addEventListener('click', function(){
+    
+})
