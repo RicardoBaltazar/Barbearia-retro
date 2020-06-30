b3b5e2e7db312c2fd29f360676efd8e1ea3323c6
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-30 15:50:45 -0300

    fazendo interações javascript

--- scripts/services.js
+++ scripts/services.js
@@ -7,5 +7,9 @@ let valueBeard = 15
 let totalValue = 0
 
 divHair.addEventListener('click', function(){
-    
+    divHair.classList.toggle('selected')
+})
+
+divBeard.addEventListener('click', function(){
+    divBeard.classList.toggle('selected')
 })
