77d46199524ac593a0975784a3d773cb45c5516f
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-01 13:21:35 -0300

    page3 layout responsivo

--- scripts/services.js
+++ scripts/services.js
@@ -1,10 +1,7 @@
 let divHair = document.querySelector('#div-hair')
 let divBeard = document.querySelector('#div-beard')
 
-let valueHair = 20
-let valueBeard = 15
 
-let totalValue = 0
 
 divHair.addEventListener('click', function(){
     divHair.classList.toggle('selected')
@@ -13,3 +10,5 @@ divHair.addEventListener('click', function(){
 divBeard.addEventListener('click', function(){
     divBeard.classList.toggle('selected')
 })
+
+console.log(choise[0])
