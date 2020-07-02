08f91c5f29aaa7345a416dc13fa2e16997337298
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-07-01 23:48:11 -0300

    modal versão 1

--- scripts/page3.js
+++ scripts/page3.js
@@ -1,86 +1,47 @@
-var hour = ''
-var name = document.querySelector('#name')
-var btn = document.querySelector("#btn")
-
+var modal = document.querySelector('.modal')
+modal.style.display = 'none'
+var hour = 'horas'
+var form = document.querySelector('form')
 
 document.querySelector('#li9').addEventListener('click', function(){
     hour = '9 horas'
     console.log(hour)
 
-    console.log(name)
+    let li9 = document.querySelector('#li9')    
+    li9.style.backgroundColor = 'grey'
+    li9.innerHTML = 'reservado'
+
+    modal.style.display = 'block'
+
 })
 
-document.querySelector('#li10').addEventListener("click", function(){
-    hour = '10 horas'
-    console.log(hour)
-})
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    var name = document.querySelector('#name')
 
-document.querySelector('#li11').addEventListener("click", function(){
-    hour = '11 horas'
-    console.log(hour)
-})
 
-document.querySelector('#li12').addEventListener("click", function(){
-    hour = '12 horas'
-    console.log(hour)
-})
+    let body = {
+        "Nome": name.value,
+        "horario": hour,
+        "telefone": '9556521'
+    }
+    
+    fetch('http://localhost:8000/horario', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log('Success: ', JSON.stringify(response))
+        })
+        .catch(error => console.error('Error:', error))
 
-document.querySelector('#li13').addEventListener("click", function(){
-    hour = '13 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li14').addEventListener("click", function(){
-    hour = '14 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li15').addEventListener("click", function(){
-    hour = '15 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li16').addEventListener("click", function(){
-    hour = '16 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li17').addEventListener("click", function(){
-    hour = '17 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li18').addEventListener("click", function(){
-    hour = '18 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li19').addEventListener("click", function(){
-    hour = '19 horas'
-    console.log(hour)
-})
-
-document.querySelector('#li20').addEventListener("click", function(){
-    hour = '20 horas'
-    console.log(hour)
+        modal.style.display = 'none'
 })
 
 
-let body = { 
-    "Nome": 'Ricardo',
-    "horario": '9 horas',
-    "telefone": '9556521'
-}
 
-fetch('http://localhost:8000/horario', {
-method: 'POST',
-headers: {'Content-Type': 'application/json'    },
-body: JSON.stringify(body),
-})
-.then(function(response){
-    return response.json();
-})
-.then(function(response){
-    console.log('Success: ', JSON.stringify(response))
-})
-.catch(error => console.error('Error:', error))
